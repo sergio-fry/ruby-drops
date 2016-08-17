d2d9c5ee800ce0cc3ac4c1bb09fb7ff463e09bbd
Sergei O. Udalov <sergei.udalov@gmail.com>
2016-08-17 21:26:13 +0300

    big refactoring in progress

--- app/controllers/drops/feed_buffer_controller.rb
+++ app/controllers/drops/feed_buffer_controller.rb
@@ -1,11 +1,10 @@
 class Drops::FeedBufferController < ApplicationController
   def index
-    @store = DropStorage.find_or_create_by(name: :feed_buffer)
+    new_items.each do |item|
+      new_items_queue.push item
+    end
 
-    @interval = params[:interval].to_i.minutes
-
-    feed = open(params[:feed_url]).read
-    @feed_doc = Nokogiri::XML(feed)
+    published_items_list.push next_item if next_item.present?
 
     render plain: <<-FEED
 <?xml version="1.0" encoding="UTF-8" ?>
@@ -14,7 +13,7 @@ class Drops::FeedBufferController < ApplicationController
   <title>Сергей Удалов / diggs</title>
   <description></description>
 
-    #{published_items.join}
+    #{published_items_list.all.join}
 </channel>
     FEED
 
@@ -22,29 +21,6 @@ class Drops::FeedBufferController < ApplicationController
   end
 
   private
-
-  def published_items
-    if last_published_at.blank?
-      [items.sort_by(&:published_at).first]
-    elsif need_more_items?
-      [items.sort_by(&:published_at).second]
-    else
-      [items.sort_by(&:published_at).first]
-    end
-  end
-
-  def need_more_items?
-    last_published_at <= @interval.ago
-  end
-
-  def last_published_at
-    @store.read(:last_published_at)
-  end
-
-  def published!
-    @store.write(:last_published_at, Time.now)
-    @store.write(:last_published_item_id, published_items.sort_by(&:published_at).last.guid)
-  end
 
   class FeedItem
     def initialize(doc)
@@ -64,7 +40,88 @@ class Drops::FeedBufferController < ApplicationController
     end
   end
 
+  class ItemsList
+    def initialize(store, name)
+      @store = store
+      @name = name
+    end
+
+    def push(item)
+      list.push item.to_s
+      save
+    end
+
+    def pop
+      res = FeedItem.new Nokogiri::XML(list.shift)
+      save
+
+      res
+    end
+
+    def last
+      list.last
+    end
+
+    def all
+      list
+    end
+
+    private
+
+    def list
+      @list ||= @store.read(@name) || []
+    end
+
+    def save
+      @store.write(@name, list)
+    end
+  end
+
+  def new_items
+    items
+  end
+
+  def new_items_queue
+    @new_items_queue ||= ItemsList.new store, :queue
+  end
+
+  def published_items_list
+    @published_items_list ||= ItemsList.new store, :published
+  end
+
+  def next_item
+    new_items_queue.pop
+  end
+
+  def need_more_items?
+    last_published_at <= interval.ago
+  end
+
+  def last_published_at
+    store.read(:last_published_at)
+  end
+
+  def published!
+    store.write(:last_published_at, Time.now)
+  end
+
   def items
-    @feed_doc.css('item').map { |item| FeedItem.new(item) }
+    feed_doc.css('item').map { |item| FeedItem.new(item) }
+  end
+
+  def feed_doc
+    Nokogiri::XML(feed_body)
+  end
+
+  def feed_body
+    open(params[:feed_url]).read
+  end
+
+  def store
+    @store ||= DropStorage.find_or_create_by(name: :feed_buffer)
+  end
+
+  def interval
+    params[:interval].to_i.minutes
   end
 end
--- app/models/drop_storage.rb
+++ app/models/drop_storage.rb
@@ -2,6 +2,8 @@ class DropStorage < ApplicationRecord
   serialize :store, Hash
   validates :name, presence: true, uniqueness: true
 
+  delegate :empty?, to: :store
+
   def write(key, value)
     store[key.to_s] = value
     save
